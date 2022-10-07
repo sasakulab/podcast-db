1d0b0be64ed2a94980267fe195d272dfe72e7409
Sasakura-ayato <sasakura.ayato.syosetu@gmail.com>
2022-10-08 08:52:38 +0900

    Add components

---
+++ html/lib/components/autocss.php
@@ -0,0 +1,7 @@
+<!--
+    if you want to read this file, you write this code.
+    include $_SERVER['DOCUMENT_ROOT']."/lib/components/autocss.php";
+-->
+
+<link href="/lib/bootstrap/default/bootstrap.min.css" rel="stylesheet">
+<script src="./lib/bootstrap/default/bootstrap.bundle.min.js"></script>
