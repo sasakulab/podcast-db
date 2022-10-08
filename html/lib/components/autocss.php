f9d3a59fa03ce391f81d59e99a80c1dfec9a82b4
Sasakura-ayato <sasakura.ayato.syosetu@gmail.com>
2022-10-08 23:58:58 +0900

    Update Custom CSS (SNS Icon Buttons)

--- html/lib/components/autocss.php
+++ html/lib/components/autocss.php
@@ -4,4 +4,5 @@
 -->
 
 <link href="/lib/bootstrap/default/bootstrap.min.css" rel="stylesheet">
-<script src="./lib/bootstrap/default/bootstrap.bundle.min.js"></script>
+<script src="/lib/bootstrap/default/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="/lib/style/custom.css">
