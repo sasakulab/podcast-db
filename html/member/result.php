c8cdcbaff21f7ba764997853a669a9ae9f31d3ab
Sasakura-ayato <sasakura.ayato.syosetu@gmail.com>
2022-10-09 10:25:41 +0900

    Replace Header Asset in member result page

--- html/member/result.php
+++ html/member/result.php
@@ -14,20 +14,7 @@
 </head>
 
 <body>
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                <h1 class="fs-4">ポットキャスト部っ！</h1>
-            </a>
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link">Search</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Member</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Program</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">News</a></li>
-                <li class="nav-item"><a href="#" class="nav-link active">Login</a></li>
-            </ul>
-        </header>
-    </div>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/lib/components/header.php"; ?>
     <div class="container">
         <div class="cover-container">
             <div class="row gx-5">
