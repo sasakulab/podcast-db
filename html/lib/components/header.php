79de7355e9f1910920839275d9e4655b80ff2cff
Sasakura-ayato <sasakura.ayato.syosetu@gmail.com>
2022-10-09 10:24:46 +0900

    Unify Header Asset

---
+++ html/lib/components/header.php
@@ -0,0 +1,18 @@
+<?php
+    # include $_SERVER['DOCUMENT_ROOT']."/lib/components/header.php";
+?>
+
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <h1 class="fs-4">ポットキャスト部っ！</h1>
+        </a>
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link">Search</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Member</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Program</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">News</a></li>
+            <li class="nav-item"><a href="#" class="nav-link active">Login</a></li>
+        </ul>
+    </header>
+</div>
