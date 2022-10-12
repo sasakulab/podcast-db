f10c0758e4d851ff809bc0b22b96db569fbfe334
Sasakura-ayato <sasakura.ayato.syosetu@gmail.com>
2022-10-12 17:47:10 +0900

    Initialize tables of SQL Database

--- html/setup/index.php
+++ html/setup/index.php
@@ -9,17 +9,19 @@
         # table: User Database
         $stmt = $pdo->prepare("
             CREATE TABLE t_user(
-            user_id INT(4) NOT NULL AUTO_INCREMENT,
-            password VARCHAR(100) NOT NULL,
-            account_id VARCHAR(20) NOT NULL,
-            create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            update_time TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL,
-            name VARCHAR(20) NULL,
-            twitter VARCHAR(20) NULL,
-            mail VARCHAR(30) NULL,
-            website VARCHAR(50) NULL,
-            comment VARCHAR(400) NULL,
-            PRIMARY KEY (user_id));         
+                user_id INT(4) NOT NULL AUTO_INCREMENT,
+                password VARCHAR(100) NOT NULL,
+                account_id VARCHAR(20) NOT NULL,
+                self_introduction VARCHAR(800) NULL,
+                create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                update_time TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL,
+                name VARCHAR(20) NOT NULL,
+                twitter VARCHAR(20) NULL,
+                mail VARCHAR(30) NULL,
+                website VARCHAR(100) NULL,
+                icon_path VARCHAR(100) NULL,
+                tag VARCHAR(25) NULL;
+                PRIMARY KEY (user_id));         
         ");
         // $stmt->bindValue(':name', $name, PDO::PARAM_STR); / 今回は変数の必要がないので利用しない
         $stmt->execute();
@@ -28,8 +30,39 @@
         $stmt = $pdo->prepare("
             CREATE TABLE t_program(
                 program_id INT(4) NOT NULL AUTO_INCREMENT,
-                owner_user_id INT (4) NOT NULL,
-                name 
+                program_cid VARCHAR(20) NOT NULL,
+                create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                update_time TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL,
+                owner_user INT(4) NOT NULL,
+                name VARCHAR(50) NOT NULL,
+                website VARCHAR(100) NULL,
+                twitter VARCHAR(20) NULL,
+                RSS VARCHAR(100) NULL,
+                pl_apple_url VARCHAR(100) NULL,
+                pl_spotify_url VARCHAR(100),
+                pl_google_url VARCHAR(100),
+                pl_yt_url VARCHAR(100) NULL,
+                pl_niconico_url VARCHAR(100) NULL,
+                description VARCHAR(800) NULL,
+                embed VARCHAR(400) NULL,
+                copylight VARCHAR(80) NULL,
+                tag VARCHAR(25) NULL,
+                PRIMARY_KEY (program_id));
+        ");
+        // $stmt->bindValue(':name', $name, PDO::PARAM_STR); / 今回は変数の必要がないので利用しない
+        $stmt->execute();
+
+        # table: News Database
+        $stmt = $pdo->prepare("
+            CREATE TABLE t_news(
+                news_id INT(8) NOT NULL AUTO_INCREMENT
+                program_id INT(4) NOT NULL,
+                create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                update_time TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL,
+                owner_user INT(4) NOT NULL,
+                tag VARCHAR(25) NULL,
+                contents VARCHAR(800) NULL,
+                PRIMARY_KEY (news_id));
         ");
         // $stmt->bindValue(':name', $name, PDO::PARAM_STR); / 今回は変数の必要がないので利用しない
         $stmt->execute();
